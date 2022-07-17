e30e671481d5b629ea08ea35b874475822c5b020
ALVARO JAVIER BECERRA GARCIA <83442694+ALJAVI20@users.noreply.github.com>
2022-07-17 11:06:28 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade de seu carro é ${vel}Km/h`)
+if(vel>=60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Vc é Brasileiro!')
+}else {
+    console.log('Vc é Extrangeiro!')
+}
